aa7411ecd5b04cdf7344c64712c9f34901f5b22e
= <=>
2014-12-05 18:41:45 +0200

    MPI in progress

--- MPI/file_io.c
+++ MPI/file_io.c
@@ -15,30 +15,40 @@
 #define M_PI 3.14159265358979323846264338327
 
 static double sRGB_to_linear(double x) {
-	if(x < 0.04045) return x/12.92;
-	return pow((x+0.055)/1.055, 2.4);
+    if(x < 0.04045) return x/12.92;
+    return pow((x+0.055)/1.055, 2.4);
 }
 
-static double GreyScaleValue(double R, double G, double B){
-	double grey_linear;
-	double R_linear, G_linear, B_linear;
+static double GrayScaleValue(double R, double G, double B){
+    double gray_linear;
+    double R_linear, G_linear, B_linear;
 
-	R_linear = sRGB_to_linear(R/255.0);
-	G_linear = sRGB_to_linear(G/255.0);
-	B_linear = sRGB_to_linear(B/255.0);
-	grey_linear = 0.299 * R_linear + 0.587 * G_linear + 0.114 * B_linear;
+    R_linear = sRGB_to_linear(R/255.0);
+    G_linear = sRGB_to_linear(G/255.0);
+    B_linear = sRGB_to_linear(B/255.0);
+    gray_linear = 0.299 * R_linear + 0.587 * G_linear + 0.114 * B_linear;
 
-	return grey_linear;
-	
+    return gray_linear;
+    
 }
 
 static double linear_to_sRGB(double y){
-	if (y <= 0.0031308) return 12.92 * y;
-	return 1.055 * pow(y, 1/2.4) - 0.055;
+    if (y <= 0.0031308) return 12.92 * y;
+    return 1.055 * pow(y, 1/2.4) - 0.055;
 }
 
-static double GreyColor(double grey_linear){
-	return round(linear_to_sRGB(grey_linear) * 255);
+static double GrayColor(double gray_linear){
+    return round(linear_to_sRGB(gray_linear) * 255);
+}
+
+static int convert_pixel_to_grayscale(int R, int G, int B){
+    
+    double gray_linear;
+    double gray_color;
+    gray_linear = GrayScaleValue((double) R, (double) G, (double) B);  
+    gray_color = GrayColor(gray_linear);
+
+    return (int) gray_color;
 }
 
 pixel_t *load_bmp(const char *filename,
@@ -91,8 +101,7 @@ pixel_t *load_bmp(const char *filename,
     }
  
     // allocate enough memory for the bitmap image data
-    pixel_t *bitmapImage = malloc(bitmapInfoHeader->bmp_bytesz *
-                                  sizeof(pixel_t));
+    pixel_t *bitmapImage = malloc(bitmapInfoHeader->width * bitmapInfoHeader->height * sizeof(pixel_t));
  
     // verify memory allocation
     if (bitmapImage == NULL) {
@@ -101,22 +110,27 @@ pixel_t *load_bmp(const char *filename,
     }
  
     // read in the bitmap image data
-    size_t pad, pad1, count=0;
+    //size_t pad;
+    size_t pad1, count=0;
     //unsigned char c;
     unsigned char c[3];
     
-    pad = 4*ceil(bitmapInfoHeader->bitspp*bitmapInfoHeader->width/32.) - bitmapInfoHeader->width;
+    //pad = 4*ceil(bitmapInfoHeader->bitspp*bitmapInfoHeader->width/32.) - bitmapInfoHeader->width;
     pad1 = 4*floor((bitmapInfoHeader->bitspp*bitmapInfoHeader->width+31)/32) - (bitmapInfoHeader->width * 3);
     for(size_t i=0; i<bitmapInfoHeader->height; i++){
-	    for(size_t j=0; j<bitmapInfoHeader->width; j++){
-		    if (fread(c, sizeof(unsigned char) * 3, 1, filePtr) != 1) {
-			    fclose(filePtr);
-			    return NULL;
-		    }
-		    bitmapImage[count++] = (short int) (1000 * GreyScaleValue(c[0], c[1], c[2]));
-	    }
-	    fseek(filePtr, pad1, SEEK_CUR);
+        for(size_t j=0; j<bitmapInfoHeader->width; j++){
+            if (fread(c, sizeof(unsigned char) * 3, 1, filePtr) != 1) {
+                fclose(filePtr);
+                return NULL;
+            }
+            bitmapImage[count++] = (short int) (convert_pixel_to_grayscale(c[2], c[1], c[0]));
+        }
+        fseek(filePtr, pad1, SEEK_CUR);
     }
+    printf("Total bmp_bytesz=%d from header\n",bitmapInfoHeader->bmp_bytesz);
+    printf("Total allocation=%d from header\n",bitmapInfoHeader->bmp_bytesz * sizeof(pixel_t));
+    printf("Total bytesz=%d width*height=%d dcount=%d\n", bitmapInfoHeader->height * bitmapInfoHeader->width * 3, 
+        bitmapInfoHeader->height * bitmapInfoHeader->width, count);
  
     // If we were using unsigned char as pixel_t, then:
     // fread(bitmapImage, 1, bitmapInfoHeader->bmp_bytesz, filePtr);
@@ -174,7 +188,7 @@ bool save_bmp(const char *filename, const bitmap_info_header_t *bmp_ih,
     // fwrite(data, 1, bmp_ih->bmp_bytesz, filePtr);
  
     // Padding: http://en.wikipedia.org/wiki/BMP_file_format#Pixel_storage
-    size_t pad = 4*ceil(bmp_ih->bitspp*bmp_ih->width/32.) - bmp_ih->width;
+    // size_t pad = 4*ceil(bmp_ih->bitspp*bmp_ih->width/32.) - bmp_ih->width;
     size_t pad1 = 4*floor((bmp_ih->bitspp*bmp_ih->width+31)/32) - (bmp_ih->width * 3);
     unsigned char c[3];
     for(size_t i=0; i < bmp_ih->height; i++) {
--- MPI/mpi_canny.c
+++ MPI/mpi_canny.c
@@ -17,25 +17,53 @@
 
 #define _DEBUG 0
 
+// pixel_t *canny_edge_detection(const pixel_t *in,
+//                               const bitmap_info_header_t *bmp_ih,
+//                               const int tmin, const int tmax,
+//                               const float sigma)
+// {
+//     const int nx = bmp_ih->width;
+//     const int ny = bmp_ih->height;
+ 
+//     pixel_t *G = calloc(nx * ny * sizeof(pixel_t), 1);
+//     pixel_t *after_Gx = calloc(nx * ny * sizeof(pixel_t), 1);
+//     pixel_t *after_Gy = calloc(nx * ny * sizeof(pixel_t), 1);
+//     pixel_t *nms = calloc(nx * ny * sizeof(pixel_t), 1);
+//     pixel_t *out = malloc(bmp_ih->bmp_bytesz * sizeof(pixel_t));
+ 
+//     if (G == NULL || after_Gx == NULL || after_Gy == NULL ||
+//         nms == NULL || out == NULL) {
+//         fprintf(stderr, "canny_edge_detection:"
+//                 " Failed memory allocation(s).\n");
+//         exit(1);
+//     }
+ 
+//     gaussian_filter(in, out, nx, ny, sigma);
+
+//     return out;
+// }
+
+
 int main(int argc, char* argv[]) {
 
 	MPI_Init(&argc, &argv); //Initialize MPI medium
 
-	int tasks, rank, resultlen, i, j;
+	int tasks, rank, resultlen;
+	// int i,j;
 	char proc_name[MPI_MAX_PROCESSOR_NAME];
-	MPI_Status stat;
+	// MPI_Status stat;
 
 	MPI_Comm_size(MPI_COMM_WORLD, & tasks);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Get_processor_name(proc_name, &resultlen);
 
-	//printf("This is thread[%d] from processor %s \n", rank, proc_name);
+	if (_DEBUG) {
+		printf("This is thread[%d] from processor %s \n", rank, proc_name);
+	}
 
 	bitmap_info_header_t ih; 
 	pixel_t* in_bitmap_data;
-	pixel_t* out_bitmap_data;
-	char* send_buffer; // Use for sending data
-	char* recv_buffer; // Use for receiving data
+	// pixel_t* out_bitmap_data;
 	int width, height; 
 	int start, stop;
 	if (rank == 0) { //Master process
@@ -43,45 +71,49 @@ int main(int argc, char* argv[]) {
     		if (in_bitmap_data == NULL) {
         		fprintf(stderr, "main: BMP image not loaded.\n");
         		return 1;
-		}
-		send_buffer = (char*)malloc(sizeof(char) * (ih.bmp_bytesz * sizeof(pixel_t)));
-		assert(send_buffer != NULL);
-
-		// Copy from in_bitmap_data to send_buffer
-		if (_DEBUG) {
-			printf("[master]%d \n", ih.bmp_bytesz);
-			printf("[master]%d %d %d\n", ih.width, ih.height, ih.width * ih.height);
-			printf("[master] %d\n", ih.bmp_bytesz * sizeof(pixel_t));
-		}
-		
-		memcpy(send_buffer, in_bitmap_data, ih.bmp_bytesz * sizeof(pixel_t));
-		
-		
-
-		int send[2];
-
-		send[0] = width  = ih.width;
-		send[1] = height = ih.height;
-
-		if (_DEBUG) {
-			printf("[Master] sending width = %d and height = %d \n", width, height );
-		}
-
-		for (i = 1; i < tasks; i++) {
-			MPI_Send(&send[0], 2, MPI_INT, i, 0, MPI_COMM_WORLD);
-		}
-
-    	} else {
-		int recv[2];
-
-		MPI_Recv(&recv, 2, MPI_INT, 0, 0, MPI_COMM_WORLD, &stat);
-		width = recv[0];
-		height = recv[1];
-		printf("Thread[%d] received width = %d and height = %d\n", rank, width, height);
-		in_bitmap_data = (pixel_t *) malloc(sizeof(pixel_t) * width * height * 3);
-
-		
+        	}
+        printf("Master[%d] read .bmp file\n", rank);
+        width = ih.width;
+        height = ih.height;
 	}
+
+	// 	// Copy from in_bitmap_data to send_buffer
+	// 	if (_DEBUG) {
+	// 		printf("[master]%d \n", ih.bmp_bytesz);
+	// 		printf("[master]%d %d %d\n", ih.width, ih.height, ih.width * ih.height);
+	// 		printf("[master] %d\n", ih.bmp_bytesz * sizeof(pixel_t));
+	// 	}
+
+	// 	int send[2];
+
+	// 	send[0] = width  = ih.width;
+	// 	send[1] = height = ih.height;
+
+	// 	if (_DEBUG) {
+	// 		printf("[Master] sending width = %d and height = %d \n", width, height );
+	// 	}
+
+	// 	for (i = 1; i < tasks; i++) {
+	// 		MPI_Send(&send[0], 2, MPI_INT, i, 0, MPI_COMM_WORLD);
+	// 	}
+
+ //    	} else {
+	// 	int recv[2];
+
+	// 	MPI_Recv(&recv, 2, MPI_INT, 0, 0, MPI_COMM_WORLD, &stat);
+	// 	width = recv[0];
+	// 	height = recv[1];
+	// 	printf("Thread[%d] received width = %d and height = %d\n", rank, width, height);
+	// 	in_bitmap_data = (pixel_t *) malloc(sizeof(pixel_t) * width * height * 3);
+
+		
+	// }
+
+	// Sending width and height
+	MPI_Bcast(&width, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&height, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	printf("Thread[%d] has width=%d and height=%d\n", rank,width, height);
 
 	int chunck = height / tasks; // Size of chunck for each process
 	int reminder = height % tasks; // Reminder if height does not divide with no reminder
@@ -90,17 +122,33 @@ int main(int argc, char* argv[]) {
 	stop  = (rank + 1) * chunck - 1; // Ending position for current chunck
 	if (rank == tasks - 1) {
 		stop += reminder;
+		chunck += reminder;
 	}
 
 	
 	printf("Thread[%d] has start = %d and stop = %d\n", rank, start, stop);
+	
 	MPI_Bcast(&ih, sizeof(bitmap_info_header_t), MPI_CHAR, 0, MPI_COMM_WORLD);
-        MPI_Bcast(in_bitmap_data, sizeof(pixel_t) * width * height * 3, MPI_CHAR, 0, MPI_COMM_WORLD);	
+	if (rank != 0) {
+		in_bitmap_data = (pixel_t*)malloc(sizeof(pixel_t) * width * height);
+		assert(in_bitmap_data != NULL);
+	}
+    MPI_Bcast(in_bitmap_data, sizeof(pixel_t) * width * height, MPI_CHAR, 0, MPI_COMM_WORLD);	
+	
 	char file[255];
 	sprintf(file, "mpi_out_%d.bmp", rank);
-    	save_bmp(file, &ih, in_bitmap_data); 
+    // save_bmp(file, &ih, in_bitmap_data); 
+
+    pixel_t* local_bitmap_data = (pixel_t*)calloc(height * width, sizeof(pixel_t));
+    assert(local_bitmap_data != NULL);
+    memcpy(local_bitmap_data, &in_bitmap_data[start*width], sizeof(pixel_t) * chunck * width);
+
+
+	// save_bmp(file, &ih, local_bitmap_data);
 
 	
+	free(in_bitmap_data);
+	free(local_bitmap_data);
 	MPI_Finalize();
 	return 0;
 }
