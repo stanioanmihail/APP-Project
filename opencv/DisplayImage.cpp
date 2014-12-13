fb0eee69a22b0851cd6a7a42505f0db5c2903f9f
Nicky <pnflavian@gmail.com>
2014-12-13 11:39:23 +0200

    Good source for canny edge in OpenCV

--- opencv/DisplayImage.cpp
+++ opencv/DisplayImage.cpp
@@ -1,28 +1,68 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
 #include <stdio.h>
-#include <opencv2/opencv.hpp>
 
 using namespace cv;
 
-int main(int argc, char** argv )
+/// Global variables
+
+Mat src, src_gray;
+Mat dst, detected_edges;
+
+int edgeThresh = 1;
+int lowThreshold;
+int const max_lowThreshold = 50;
+int ratio = 3;
+int kernel_size = 3;
+char window_name[] = "Edge Map";
+
+/**
+ * @function CannyThreshold
+ * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
+ */
+void CannyThreshold(int, void*)
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
+  /// Reduce noise with a kernel 5x5
+  blur( src_gray, detected_edges, Size(5,5) );
 
-    Mat image;
-    image = imread( argv[1], 1 );
+  /// Canny detector
+  Canny( detected_edges, detected_edges, lowThreshold, 50, kernel_size );
 
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_NORMAL );
-    imshow("Display Image", image);
+  /// Using Canny's output as a mask, we display our result
+  dst = Scalar::all(0);
 
-    waitKey(0);
+  src_gray.copyTo( dst, detected_edges);
+  imshow( window_name, dst );
+ }
 
-    return 0;
-}
+
+/** @function main */
+int main( int argc, char** argv )
+{
+  /// Load an image
+  src = imread( argv[1] );
+
+  if( !src.data )
+  { return -1; }
+
+  /// Create a matrix of the same type and size as src (for dst)
+  dst.create( src.size(), src.type() );
+
+  /// Convert the image to grayscale
+  cvtColor( src, src_gray, CV_BGR2GRAY );
+
+  /// Create a window
+  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+
+  /// Create a Trackbar for user to enter threshold
+  createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
+
+  /// Show the image
+  CannyThreshold(0, 0);
+
+  /// Wait until user exit program by pressing a key
+  waitKey(0);
+
+  return 0;
+  }
